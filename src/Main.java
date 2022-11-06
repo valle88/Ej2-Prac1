6554442c839714c55590cf3fda9d18bd59fe7dc6
valle <vallukis@gmail.com>
2022-11-06 17:01:21 +0000

    intanciar las clases creadas anteriormente

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Mascotas PUA = new Mascotas("PUA", 4, Mascotas.GENERO.FEMENINO,2);
+        Serpiente LUPE = new Serpiente("lupe",0, Mascotas.GENERO.FEMENINO, 40);
+        Pantera PIPOL = new Pantera("PIPOL",4, Mascotas.GENERO.MASCULINO,32);
+
+        System.out.println(PUA);
+        System.out.println(LUPE);
+        System.out.println(PIPOL);
     }
 }
